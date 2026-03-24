a1b11714691d50ae147599cc365ebf10cfed3e7a
Khushi Choyal <khushichoyal16l@gmail.com>
2026-03-24 20:13:08 +0530

    back-url

--- bank-dashboard/src/services/api.js
+++ bank-dashboard/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/accounts",
+  baseURL: "https://bankmanagementsystem-3.onrender.com/api/accounts",
 });
 
 export const getAccounts = () => API.get("/");
